4d510c646efcd6f5e7ad1b79b59322473065bbcb
Davi Ribeiro <foxpc18@gmail.com>
2021-11-14 22:03:00 -0300

    Insert SOIL

--- teste.c
+++ teste.c
@@ -1,34 +1,176 @@
-// TeaPot3D.c - Isabel H. Manssour
-// Um programa OpenGL que exemplifica a visualização
-// de objetos 3D.
-// Este código está baseado nos exemplos disponíveis no livro
-// "OpenGL SuperBible", 2nd Edition, de Richard S. e Wright Jr.
 
-#include <GL/glut.h>
-#include <stdlib.h>
-#include <math.h>
+#include <gl/glut.h>
+#include <stdio.h>
+#include "glaux.h"
 
-GLfloat angle, fAspect;
+//Função para Carregar uma imagem .BMP
+AUX_RGBImageRec *LoadBMP(char *Filename)
+{
+  FILE *File = NULL;
+  if (!Filename)
+  {
+    return NULL;
+  }
+  File = fopen(Filename, "r");
+  if (File) // Se o arquivo existe
+  {
+    fclose(File);
+    return auxDIBImageLoad(Filename); //Retorna a imagem
+  }
+  return NULL;
+}
+
+// Variáveis globais
+GLfloat angle, fAspect, rotX, rotY, obsZ;
+GLuint idTextura;
+AUX_RGBImageRec *imagemTextura;
+
+// Função responsável pela especificação dos parâmetros de iluminação
+void DefineIluminacao(void)
+{
+  GLfloat luzAmbiente[4] = {0.2, 0.2, 0.2, 1.0};
+  GLfloat luzDifusa[4] = {0.7, 0.7, 0.7, 1.0};    // "cor"
+  GLfloat luzEspecular[4] = {1.0, 1.0, 1.0, 1.0}; // "brilho"
+  GLfloat posicaoLuz[4] = {0.0, -40.0, 0.0, 1.0};
+
+  // Capacidade de brilho do material
+  GLfloat especularidade[4] = {1.0, 1.0, 1.0, 1.0};
+  GLint especMaterial = 60;
+
+  // Define a refletância do material
+  glMaterialfv(GL_FRONT, GL_SPECULAR, especularidade);
+  // Define a concentração do brilho
+  glMateriali(GL_FRONT, GL_SHININESS, especMaterial);
+
+  // Ativa o uso da luz ambiente
+  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luzAmbiente);
+
+  // Define os parâmetros da luz de número 0
+  glLightfv(GL_LIGHT0, GL_AMBIENT, luzAmbiente);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, luzDifusa);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, luzEspecular);
+  glLightfv(GL_LIGHT0, GL_POSITION, posicaoLuz);
+
+  // Habilita o modelo de colorização de Gouraud
+  glShadeModel(GL_SMOOTH);
+}
 
 // Função callback chamada para fazer o desenho
 void Desenha(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+  // Limpa a janela e o depth buffer
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glColor3f(0.0f, 0.0f, 1.0f);
+  // Desenha um cubo no qual a textura é aplicada
+  glBegin(GL_QUADS);
+  // Face frontal
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  // Face posterior
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f, -1.0f);
+  // Face superior
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  // Face inferior
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(1.0f, -1.0f, -1.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  // Face lateral direita
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f, -1.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, -1.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(1.0f, 1.0f, 1.0f);
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(1.0f, -1.0f, 1.0f);
+  // Face lateral esquerda
+  glTexCoord2f(0.0f, 0.0f);
+  glVertex3f(-1.0f, -1.0f, -1.0f);
+  glTexCoord2f(1.0f, 0.0f);
+  glVertex3f(-1.0f, -1.0f, 1.0f);
+  glTexCoord2f(1.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, 1.0f);
+  glTexCoord2f(0.0f, 1.0f);
+  glVertex3f(-1.0f, 1.0f, -1.0f);
+  glEnd();
 
-  // Desenha o teapot com a cor corrente (wire-frame)
-  glutWireTeapot(50.0f);
-
-  // Executa os comandos OpenGL
   glutSwapBuffers();
+}
+
+// Função usada para especificar a posição do observador virtual
+void PosicionaObservador(void)
+{
+  // Especifica sistema de coordenadas do modelo
+  glMatrixMode(GL_MODELVIEW);
+  // Inicializa sistema de coordenadas do modelo
+  glLoadIdentity();
+  DefineIluminacao();
+  // Especifica posição do observador e do alvo
+  glTranslatef(0, 0, -obsZ);
+  glRotatef(rotX, 1, 0, 0);
+  glRotatef(rotY, 0, 1, 0);
 }
 
 // Inicializa parâmetros de rendering
 void Inicializa(void)
 {
+  // Define a cor de fundo da janela de visualização como preta
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-  angle = 45;
+
+  // Habilita a definição da cor do material a partir da cor corrente
+  glEnable(GL_COLOR_MATERIAL);
+  //Habilita o uso de iluminação
+  glEnable(GL_LIGHTING);
+  // Habilita a luz de número 0
+  glEnable(GL_LIGHT0);
+  // Habilita o depth-buffering
+  glEnable(GL_DEPTH_TEST);
+
+  // Inicializa a variável que especifica o ângulo da projeção
+  // perspectiva
+  angle = 50;
+
+  // Inicializa as variáveis usadas para alterar a posição do
+  // observador virtual
+  rotX = 30;
+  rotY = 0;
+  obsZ = 10;
+
+  // Comandos de inicialização para textura
+  imagemTextura = LoadBMP("imagem.bmp");
+  glGenTextures(1, &idTextura);
+  glBindTexture(GL_TEXTURE_2D, idTextura);
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, imagemTextura->sizeX,
+               imagemTextura->sizeY, 0, GL_RGB, GL_UNSIGNED_BYTE,
+               imagemTextura->data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glEnable(GL_TEXTURE_2D);
 }
 
 // Função usada para especificar o volume de visualização
@@ -39,16 +181,10 @@ void EspecificaParametrosVisualizacao(void)
   // Inicializa sistema de coordenadas de projeção
   glLoadIdentity();
 
-  // Especifica a projeção perspectiva
-  gluPerspective(angle, fAspect, 0.1, 500);
+  // Especifica a projeção perspectiva(angulo,aspecto,zMin,zMax)
+  gluPerspective(angle, fAspect, 0.5, 500);
 
-  // Especifica sistema de coordenadas do modelo
-  glMatrixMode(GL_MODELVIEW);
-  // Inicializa sistema de coordenadas do modelo
-  glLoadIdentity();
-
-  // Especifica posição do observador e do alvo
-  gluLookAt(0, 80, 200, 0, 0, 0, 0, 1, 0);
+  PosicionaObservador();
 }
 
 // Função callback chamada quando o tamanho da janela é alterado
@@ -58,7 +194,7 @@ void AlteraTamanhoJanela(GLsizei w, GLsizei h)
   if (h == 0)
     h = 1;
 
-  // Especifica o tamanho da viewport
+  // Especifica as dimensões da viewport
   glViewport(0, 0, w, h);
 
   // Calcula a correção de aspecto
@@ -86,16 +222,51 @@ void GerenciaMouse(int button, int state, int x, int y)
   glutPostRedisplay();
 }
 
-// Programa Principal
-int main(int argc, char **argv)
+// Callback para gerenciar eventos do teclado para teclas especiais
+void TeclasEspeciais(int key, int x, int y)
 {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  glutInitWindowSize(350, 300);
-  glutCreateWindow("Visualizacao 3D");
+  switch (key)
+  {
+  case GLUT_KEY_LEFT:
+    rotY--;
+    break;
+  case GLUT_KEY_RIGHT:
+    rotY++;
+    break;
+  case GLUT_KEY_UP:
+    rotX++;
+    break;
+  case GLUT_KEY_DOWN:
+    rotX--;
+    break;
+  case GLUT_KEY_HOME:
+    obsZ++;
+    break;
+  case GLUT_KEY_END:
+    obsZ--;
+    break;
+  case GLUT_KEY_F10: // "camera reset"
+    rotX = 30;
+    rotY = 0;
+    obsZ = 10;
+    angle = 50;
+    EspecificaParametrosVisualizacao();
+    break;
+  }
+  PosicionaObservador();
+  glutPostRedisplay();
+}
+
+// Programa Principal
+int main(void)
+{
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(400, 350);
+  glutCreateWindow("Visualizacao 3D com Mapeamento de Textura");
   glutDisplayFunc(Desenha);
   glutReshapeFunc(AlteraTamanhoJanela);
   glutMouseFunc(GerenciaMouse);
+  glutSpecialFunc(TeclasEspeciais);
   Inicializa();
   glutMainLoop();
 }
